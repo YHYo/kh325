2fe3911c22be37c791129ed56b6235fd5b367882
YHYo <yhy7235@naver.com>
2022-07-30 17:23:04 +0900

    궁종묘 class 이름 변경

---
+++ work_space_git/semi_heritage/src/main/java/semi/heritage/palace/vo/PalaceURL.java
@@ -0,0 +1,29 @@
+package semi.heritage.palace.vo;
+
+public class PalaceURL {
+
+	private String link;
+
+	public PalaceURL() {
+		super();
+	}
+
+	public PalaceURL(String link) {
+		super();
+		this.link = link;
+	}
+
+	@Override
+	public String toString() {
+		return "Palace_Jongmyo_link [link=" + link + "]";
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+}
