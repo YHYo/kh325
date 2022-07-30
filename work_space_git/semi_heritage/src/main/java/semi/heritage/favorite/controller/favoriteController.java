495416a2705aed0ddc929fcc521bf44a58fa720c
Jeongwonil <darkoasis2@naver.com>
2022-07-30 19:00:20 +0900

    찜 기능 수정 20220730 19:00 by 정원일

--- work_space_git/semi_heritage/src/main/java/semi/heritage/favorite/controller/favoriteController.java
+++ work_space_git/semi_heritage/src/main/java/semi/heritage/favorite/controller/favoriteController.java
@@ -3,6 +3,7 @@ package semi.heritage.favorite.controller;
 import java.util.List;
 
 import semi.heritage.favorite.service.favoriteService;
+import semi.heritage.favorite.vo.favoriteMyPageVO;
 import semi.heritage.favorite.vo.favoritePalaceVO;
 import semi.heritage.favorite.vo.favoriteVO;
 
@@ -10,10 +11,21 @@ public class favoriteController {
 	
 	favoriteService fs = new favoriteService();
 	
-	public List<favoriteVO> sortedByFavorite(){
-		return fs.sortedByFavorite();
+	// 찜 insert
+	public int insert(int uNo, int no) {
+		return fs.insert(uNo, no);
+	}
+
+	// 찜 delete
+	public int delete(int uNo, int no) {
+		return fs.delete(uNo, no);
 	}
 	
+	public List<favoriteMyPageVO> selectAll(int uNo) {
+		return fs.selectAll(uNo);
+	}
+	
+	// 문화재 번호로 찜 개수 구하기
 	public List<favoriteVO> CountFavoriteByNo(int no){
 		return fs.CountFavoriteByNo(no);
 	}
--- work_space_git/semi_heritage/src/main/java/semi/heritage/favorite/dao/favoriteDao.java
+++ work_space_git/semi_heritage/src/main/java/semi/heritage/favorite/dao/favoriteDao.java
@@ -8,36 +8,79 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
+import semi.heritage.favorite.vo.favoriteMyPageVO;
 import semi.heritage.favorite.vo.favoriteVO;
 
 public class favoriteDao {
 
-	public List<favoriteVO> sortedByFavorite(Connection conn) {
-		List<favoriteVO> list = new ArrayList<>();
+	public int insert(Connection conn, int uNo, int no) {
+		
+		// 일반 문화재 찜 추가
+		try {
+			String sql = "INSERT INTO hFavorite VALUES(SEQ_hfavNum.NEXTVAL, ?, ?)";
+			
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+
+			pstmt.setInt(1, uNo);
+			pstmt.setInt(2, no);
+
+			int result = pstmt.executeUpdate();
+			pstmt.close();
+			return result;
+		} catch (Exception e) {
+		}
+		return -1;
+	}
+
+	// 일반 문화재 찜 삭제
+	public int delete(Connection conn, int uNo, int no) {// 즐겨찾기 정보 삽입시 회원번호랑 마번 입력받아서 정보 삭제
+		try {
+			String sql = "DELETE FROM hFavorite WHERE uNo = ? AND no = ? ";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+
+			pstmt.setInt(1, uNo);
+			pstmt.setInt(2, no);
+
+			int result = pstmt.executeUpdate();
+			pstmt.close();
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1;
+	}
+	
+	// 마이페이지에서 찜목록 출력
+	public List<favoriteMyPageVO> selectAll(Connection conn, int uNo) {
+		List<favoriteMyPageVO> list = new ArrayList<>();
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 
 		try {
-			String sql = "SELECT ROWNUM, FV.*"
-					   + " FROM(SELECT no from favorite group by no order by COUNT(uno) desc) FV"
-					   + " WHERE ROWNUM BETWEEN 1 AND 10";
-
+			String sql = "SELECT "
+					+ "HFV.hfavNum, H.CCMANAME, H.CCBAMNM1, H.CCBALCAD, H.IMAGEURL, H.CONTENT "
+					+ "FROM "
+					+ "heritage H, hFavorite HFV "
+					+ "WHERE "
+					+ "H.no = HFV.no AND "
+					+ "HFV.uNO = ? "
+					+ "order by hfavNum desc"; 
+																														
 			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, uNo);
 			rs = pstmt.executeQuery();
 
-			while (rs.next()) {
-				int count = 1;
-				int rowNum = rs.getInt(count++);
-				int no = rs.getInt(count++);
-
-				favoriteVO favRank = new favoriteVO(rowNum, no);
-				list.add(favRank);
-				
-				no = list.get(0).getNo();
-				System.out.println(no);
+			while (rs.next() == true) {
+				int hfavNum = rs.getInt("hfavNum");
+				String ccmaName = rs.getString("ccmaName");
+				String ccbaMnm1 = rs.getString("ccbaMnm1");
+				String ccbaLcad = rs.getString("ccbaLcad");
+				String imageUrl = rs.getString("imageUrl");
+				String content = rs.getString("content");
+				favoriteMyPageVO mypage = new favoriteMyPageVO(hfavNum, ccmaName, ccbaMnm1, ccbaLcad, imageUrl, content);
+				list.add(mypage);
 			}
 			return list;
-		
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -47,6 +90,7 @@ public class favoriteDao {
 		return null;
 	}
 	
+	// 문화재 번호로 해당 찜 개수 구하기
 	public List<favoriteVO> CountFavoriteByNo(Connection conn, int no) {
 		List<favoriteVO> list = new ArrayList<>();
 		PreparedStatement pstmt = null;
